80ba4e7f69688c3184a1e6be0c0eaefa073b711d
Helia-Nik <healia@m.lo.agency>
2021-11-08 19:21:41 +0330

    feat: config prettier and eslint

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+Sentry.init({
+	dsn: process.env.REACT_APP_DSN_URL,
+	integrations: [new Integrations.BrowserTracing()],
+	tracesSampleRate: 1.0
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
