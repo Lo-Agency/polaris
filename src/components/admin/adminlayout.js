7df0c2910afe71f37497e438763ed0195137ff54
Sepideh <sepideh@m.lo.agency>
2021-11-26 20:31:11 +0330

    feat: add new colors

--- src/components/admin/adminlayout.js
+++ src/components/admin/adminlayout.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router"
 import { Link } from "react-router-dom"
 import config from "../../util/config";
 
+
 const Adminlayout = ({ children }) => {
     const { entityName, actionName } = useParams()
 
