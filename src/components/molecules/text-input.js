40b7df82f5a93ca4e8236737cbacfe93c4c2965b
Yasaman K <yasaman@m.lo.agency>
2022-01-26 16:34:45 +0330

    feat: entity form ui

--- src/components/molecules/text-input.js
+++ src/components/molecules/text-input.js
@@ -5,7 +5,7 @@ const TextInput = ({ name, formValues }) => {
         <div key={name} className="w-6/12 flex items-center flex-col justify-center px-8 ">
             <label className="mx-2 self-start">{title(name)}:
             </label>
-            <input className=" py-2 my-3 w-full rounded-none border-solid border-gray-300 border" name={name} type="text" defaultValue={formValues && formValues[name]} required />
+            <input className="p-2 my-3 w-full rounded-none border-solid border-gray-300 border" name={name} type="text" defaultValue={formValues && formValues[name]} required />
         </div>
     )
 }
