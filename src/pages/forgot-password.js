692d438773cb69466ccbfca7e3a7bdaa27597b14
Yasaman K <yasaman@m.lo.agency>
2022-01-28 02:18:37 +0330

    feat: add loading btn

--- src/pages/forgot-password.js
+++ src/pages/forgot-password.js
@@ -4,9 +4,11 @@ import AuthLayout from "../components/layouts/auth-layout"
 
 function ForgotPassword() {
 	const auth = useAuth();
+	const [loading, setLoading] = useState(false);
 	const [error, setAllert] = useState();
 
 	const sendResetPassEmail = async (event) => {
+		setLoading(true)
 		event.preventDefault();
 		const formData = new FormData(event.currentTarget);
 		const email = formData.get("email");
@@ -17,6 +19,7 @@ function ForgotPassword() {
 		} catch (e) {
 			setAllert(e.message)
 		}
+		setLoading(false)
 	};
 
 	return (
@@ -31,9 +34,26 @@ function ForgotPassword() {
 						<input className="py-2 px-3 xsm:w-48 sm:w-60 w-80 border-2 border-black" name="email" type="email" />
 					</div>
 
-					<button className="btn-form" type="submit">
-						Reset password
+					<button className="btn-form w-2/12 flex justify-center items-center" type="submit">
+						{loading ?
+							<svg width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" className="mr-3" stroke="#fff">
+								<g transform="translate(1 1)" strokeWidth="2">
+									<circle opacity=".5" cx="18" cy="18" r="18" />
+									<path d="M36 18c0-9.94-8.06-18-18-18">
+										<animateTransform
+											attributeName="transform"
+											type="rotate"
+											from="0 18 18"
+											to="360 18 18"
+											dur="1s"
+											repeatCount="indefinite" />
+									</path>
+								</g>
+							</svg>
+							: <>Reset password</>
+						}
 					</button>
+
 
 					<a href='/login' className="py-2">Login</a>
 					{error && <div className="flex items-center text-blue-500 text-sm font-bold px-4 py-3" role="alert">
--- src/pages/login.js
+++ src/pages/login.js
@@ -6,9 +6,11 @@ import AuthLayout from "../components/layouts/auth-layout";
 function Login() {
 	const navigate = useNavigate();
 	const auth = useAuth();
+	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(null);
 
 	const handleSubmit = async (event) => {
+		setLoading(true)
 		event.preventDefault();
 		const formData = new FormData(event.currentTarget);
 		const email = formData.get("email");
@@ -20,6 +22,7 @@ function Login() {
 		} catch (e) {
 			setError(e.message)
 		}
+		setLoading(false)
 	};
 
 	return (
@@ -39,7 +42,28 @@ function Login() {
 						<input className="py-2 px-3 xsm:w-48 sm:w-60 w-80 border-2 border-black" name="password" type="password" />
 					</div>
 
-					<button className="mt-8 btn-form" type="submit">Login</button>
+					<button className="btn-form w-2/12 flex justify-center items-center" type="submit">
+						{loading ?
+							<svg width="38" height="38" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg" className="mr-3" stroke="#fff">
+								<g transform="translate(1 1)" strokeWidth="2">
+									<circle opacity=".5" cx="18" cy="18" r="18" />
+									<path d="M36 18c0-9.94-8.06-18-18-18">
+										<animateTransform
+											attributeName="transform"
+											type="rotate"
+											from="0 18 18"
+											to="360 18 18"
+											dur="1s"
+											repeatCount="indefinite" />
+									</path>
+								</g>
+							</svg>
+							: <>Login</>
+						}
+					</button>
+
+
+
 					<a href='/forgot-password' className="py-2">Forgot Password</a>
 					<a href='/signup'>Dont have an account yet?</a>
 					{error && <div className="flex items-center text-red-500 text-sm font-bold px-4 py-3" role="alert">
