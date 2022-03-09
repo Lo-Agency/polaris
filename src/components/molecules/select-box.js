9e8fb6bb33eec8cc946eb42900fbb52360aeb87f
Yasaman K <yasaman@m.lo.agency>
2022-03-09 13:06:20 +0330

    fix: remove unused code

--- src/components/molecules/select-box.js
+++ src/components/molecules/select-box.js
@@ -3,24 +3,10 @@ import Select from 'react-select';
 import makeAnimated from 'react-select/animated';
 import { title } from 'case';
 import LoadingPage from './loading-page';
-import { extractDataFromEntity } from '../../util/extract-data';
-import { useCrud } from '../providers/crud.provider';
 
 const SelectBox = ({ name, entityName, formValues, actionName }) => {
 	const animatedComponents = makeAnimated();
-	const crud = useCrud();
-	const dataState = crud.dataState;
-	const categories = extractDataFromEntity('category', dataState);
-
-	let selectvalue;
-	let categoryarray = [];
-	if (config.entities[entityName].fields[name].value) {
-		selectvalue = config.entities[entityName].fields[name].value;
-	} else {
-		Object.values(categories).map((x) => categoryarray.push(x['title']));
-		selectvalue = categoryarray;
-	}
-
+	const selectvalue = config.entities[entityName].fields[name].value;
 	const selectOptions = selectvalue && selectvalue.map((option) => ({ value: option, label: option }));
 	const defaultValue = formValues && selectOptions.filter((option) => option['value'] === formValues[name]);
 	const showForm = defaultValue || actionName === 'create';
