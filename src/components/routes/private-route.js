aaee88778302b50dd639a80da913a867902a27f4
Yasaman K <yasaman@m.lo.agency>
2022-04-05 09:51:08 +0430

    feat: remove commented code

--- src/components/routes/private-route.js
+++ src/components/routes/private-route.js
@@ -1,6 +1,3 @@
-// import { Navigate, useLocation } from 'react-router-dom';
-// import config from '../../util/config';
-// import { useAuth } from '../providers/auth.provider';
 import 'react-toastify/dist/ReactToastify.css';
 
 function PrivateRoute({ children }) {
