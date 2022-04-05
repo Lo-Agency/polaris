56d7067b0bf76fa841038eeacebad626315da06e
Yasaman K <yasaman@m.lo.agency>
2022-04-05 11:01:27 +0430

    fix: private route

--- src/components/routes/private-route.js
+++ src/components/routes/private-route.js
@@ -1,6 +1,11 @@
+import { Navigate } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
+import config from '../../util/config';
+import { useAuth } from '../providers/auth.provider';
 
 function PrivateRoute({ children }) {
+	const { user } = useAuth();
+	if (!user) return <Navigate to={config.routes.landing.pathname} state={{ from: location }} />;
 	return children;
 }
 
