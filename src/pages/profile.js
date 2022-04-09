63d9636c52d087cf8f4693aa10884cd3de76eebb
helia <heliahnik77@gmail.com>
2022-04-09 12:35:29 +0430

    fix: profile page

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -13,7 +13,6 @@ const Profile = () => {
 	const saveProfile = () => {
 		const form = new FormData(event.target);
 		const values = {
-			email: form.get('email'),
 			firstname: form.get('firstname'),
 			lastname: form.get('lastname'),
 			workspacename: form.get('workspacename'),
@@ -45,16 +44,6 @@ const Profile = () => {
 								name="lastname"
 								type="text"
 								defaultValue={userMetaData['lastname']}
-							/>
-						</div>
-
-						<div className="m-2 sm:m-1 xsm:m-1 flex flex-col">
-							<label className="py-2">Email:</label>
-							<input
-								className="py-2 px-3 outline-none xsm:w-48 sm:w-60 w-72 border-2 border-black"
-								name="email"
-								type="email"
-								defaultValue={userMetaData['email']}
 							/>
 						</div>
 
