44885ff40461d33aa276b9983e1944a927413e16
Yasaman K <yasaman@m.lo.agency>
2022-04-12 16:17:37 +0430

    feat: add profile button to home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -10,7 +10,21 @@ export function Home() {
 			{user ? (
 				<header className="navbar fixed w-full px-4">
 					<Link className="text-2xl" to={`/${user.uid}/${user.uid}`}>
-						dashboard
+						Dashboard
+					</Link>
+					<Link className="px-4" to={`/${user.uid}/profile`}>
+						<svg
+							className="w-6 h-6"
+							fill="none"
+							stroke="currentColor"
+							viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg">
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
+						</svg>
 					</Link>
 				</header>
 			) : (
